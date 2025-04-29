f3d7820843385e43975da54c48d0384353028ed1
teddy-harfa <teddyharfa@gmail.com>
2025-04-29 16:49:42 +0700

    update push ke github packages

--- build_push_image.sh
+++ build_push_image.sh
@@ -2,25 +2,25 @@
 
 #build image item-app dengan tag v1 dari Dockerfile
 echo "-------------------------------------------------------------"
-echo "Build image....\n"
+echo -e "Build image....\n"
 docker build -t item-app:v1 .
 
 #list image yang ada pada lokal
 echo "-------------------------------------------------------------"
-echo "Daftar Image di lokal:\n"
+echo -e "Daftar Image di lokal:\n"
 docker images
 
-#rubah nama image agar sesuai dengan format docker hub
+#rubah nama image agar sesuai dengan format git package
 echo "-------------------------------------------------------------"
-echo "Rubah nama image\n"
-docker tag item-app:v1 tmasbro99/item-app:v1
+echo -e "Rubah nama image\n"
+docker tag item-app:v1 ghcr.io/teddy-harfa/item-app:v1
 
-#login ke docker hub
+#login ke git package
 echo "-------------------------------------------------------------"
-echo "Login ke docker hub...\n"
-echo $PASSWORD_DOCKER_HUB | docker login -u tmasbro99 --password-stdin
+echo -e "Login ke git package...\n"
+echo $CR_PAT | docker login ghcr.io -u teddy-harfa --password-stdin
 
-#push ke docker hub
+#push ke git package
 echo "-------------------------------------------------------------"
-echo "Unggah image ke docker hub\n"
-docker push tmasbro99/item-app:v1
+echo -e "Unggah image ke git package\n"
+docker push ghcr.io/teddy-harfa/item-app:v1
