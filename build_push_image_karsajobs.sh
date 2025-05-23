0a134a254a65f0dbf66edb65e559285ce0567337
teddy-harfa <teddyharfa@gmail.com>
2025-05-23 14:29:02 +0700

    update karsajobs

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+#build image karsajobs dengan tag latest dari Dockerfile
+echo "-------------------------------------------------------------"
+echo -e "Build image....\n"
+docker build -t ghcr.io/teddy-harfa/karsajobs:latest .
+
+#list image karsajobs
+echo "-------------------------------------------------------------"
+echo -e "Daftar Image karsajobs di lokal:\n"
+docker images | grep 'karsajobs'
+
+#login ke git package
+echo "-------------------------------------------------------------"
+echo -e "Login ke git package...\n"
+echo $CR_PAT | docker login ghcr.io -u teddy-harfa --password-stdin
+
+#push ke git package
+echo "-------------------------------------------------------------"
+echo -e "Unggah image ke git package\n"
+docker push ghcr.io/teddy-harfa/karsajobs:latest
